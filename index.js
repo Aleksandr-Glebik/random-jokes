5215125c3f1c60bb1cd27fb631c30f40e3165009
Aleksandr-Glebik <glebikaa@mail.ru>
2022-03-27 00:27:11 +0300

    feat: add translate.js, add eventListener for headerBlock, add func setLanguages

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 // const url = 'https://api.icndb.com/jokes'
+import i18Obj from './translate.js';
 
 const mainText = document.querySelector('.main-text')
 const mainTextAuthor = document.querySelector('.main-text-author')
 const btnMain = document.querySelector('.btn-main')
 const img = document.querySelector('.main-img')
+const headerBlock = document.querySelector('.header-block')
+
+let lang = 'en'
 
 btnMain.addEventListener('click', getData)
 btnMain.addEventListener('click', addAnimation)
+headerBlock.addEventListener('click', setLanguages)
 
 async function getData() {
     const url = 'https://type.fit/api/quotes'
@@ -55,4 +60,28 @@ function addAnimation() {
         mainText.classList.remove('active-text')
         mainTextAuthor.classList.remove('active-text')
     }, 600)
+}
+
+// console.log('i18Obj', i18Obj);
+// console.log('Object.keys(i18Obj)', Object.keys(i18Obj))
+// console.log('Object.keys(i18Obj)', Object.values(i18Obj))
+
+function setLanguages(event) {
+    if (event.target.classList.contains('btn-languages')) {
+        let laguage = event.target.textContent
+        // console.log('laguage', laguage);
+        if (laguage === 'en') {
+            lang = 'en'
+            // console.log('i18Obj', i18Obj[lang]);
+            getTranslate(i18Obj[lang])
+        } else if (laguage === 'ru') {
+            lang = 'ru'
+            // console.log('i18Obj', i18Obj[lang]);
+            getTranslate(i18Obj[lang])
+        }
+    }
+}
+
+function getTranslate(obj) {
+    console.log('obj', obj);
 }
---
+++ translate.js
@@ -0,0 +1,14 @@
+const i18Obj = {
+    'en': {
+      'header-text': 'Random Jokes',
+      'btn-main': 'Click me!',
+      'footer-link': 'Aleksandr-Glebik',
+    },
+    'ru': {
+      'header-text': 'Случайные шутки',
+      'btn-main': 'Нажми на меня!',
+      'footer-link': 'Александр-Глебик',
+    }
+  }
+  
+  export default i18Obj;
