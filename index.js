c5d621c96c8a28649d911e68c6ca57b1a436c8fc
Aleksandr-Glebik <glebikaa@mail.ru>
2022-03-26 23:50:56 +0300

    feat: add animation for text and img, add func addAnimation

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
 const mainText = document.querySelector('.main-text')
 const mainTextAuthor = document.querySelector('.main-text-author')
 const btnMain = document.querySelector('.btn-main')
+const img = document.querySelector('.main-img')
 
 btnMain.addEventListener('click', getData)
+btnMain.addEventListener('click', addAnimation)
 
 async function getData() {
     const url = 'https://type.fit/api/quotes'
@@ -42,4 +44,15 @@ function displayError(err) {
     // console.log('err', err);
     mainText.textContent = `Sorry, try again, error: ${err}"`
     mainTextAuthor.textContent = `Sorry, try again!`
+}
+
+function addAnimation() {
+    img.classList.add('active')
+    mainText.classList.add('active-text')
+    mainTextAuthor.classList.add('active-text')
+    setTimeout(() => {
+        img.classList.remove('active')
+        mainText.classList.remove('active-text')
+        mainTextAuthor.classList.remove('active-text')
+    }, 600)
 }
