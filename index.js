5f2e2517280a465f31da68a48c24eb0a67497f52
Aleksandr-Glebik <glebikaa@mail.ru>
2022-03-27 02:18:14 +0300

    fix: clean code from comments in js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// const url = 'https://api.icndb.com/jokes'
 import i18Obj from './translate.js';
 
 const mainText = document.querySelector('.main-text')
@@ -10,7 +9,6 @@ const headerText = document.querySelector('.header-text')
 
 let lang = 'en'
 
-// btnMain.addEventListener('click', getData)
 btnMain.addEventListener('click', addAnimation)
 headerBlock.addEventListener('click', setLanguages)
 
@@ -19,13 +17,11 @@ async function getData() {
     try {
         const res = await fetch(url)
         const data = await res.json()
-        // console.log(data)
         getRandomObj(data)
     } catch(err) {
         displayError(err)
     }
 }
-// getData()
 
 setTimeout(() => {
     if (lang === 'en') {
@@ -37,28 +33,22 @@ setTimeout(() => {
     }
 }, 100)
 
-
-
 function displayText(value) {
     mainText.textContent = `"${value.text}"`
     mainTextAuthor.textContent = value.author
 }
 
 function getRandomObj(data) {
-    // console.log('data', data)
     let ind = Math.round(Math.random() * (data.length))
-    // console.log('ind', ind);
     let randomObj = data[ind]
     if (randomObj.author === null) {
         getRandomObj(data)
     } else {
-        // console.log('randomObj', randomObj)
         displayText(randomObj)
     }
 }
 
 function displayError(err) {
-    // console.log('err', err);
     mainText.textContent = `Sorry, try again, error: ${err}"`
     mainTextAuthor.textContent = `Sorry, try again!`
 }
@@ -76,25 +66,16 @@ function addAnimation() {
     }, 600)
 }
 
-// console.log('i18Obj', i18Obj);
-// console.log('Object.keys(i18Obj)', Object.keys(i18Obj))
-// console.log('Object.keys(i18Obj)', Object.values(i18Obj))
-
 function setLanguages(event) {
     if (event.target.classList.contains('btn-languages')) {
         let laguage = event.target.textContent
-        // console.log('laguage', laguage);
         if (laguage === 'en') {
             lang = 'en'
-            // console.log('i18Obj', i18Obj[lang]);
             getTranslate(i18Obj[lang])
-            console.log('lang', lang);
             addListenerForEnText()
         } else if (laguage === 'ru') {
             lang = 'ru'
-            // console.log('i18Obj', i18Obj[lang]);
             getTranslate(i18Obj[lang])
-            console.log('lang', lang);
             addListenerForRuText()
         }
     }
@@ -114,11 +95,8 @@ async function getQuotes() {
     const quotes = 'quotes.json';
     const res2 = await fetch(quotes);
     const data2 = await res2.json();
-    // console.log(data2);
     getRandomObj(data2)
 }
-
-// getQuotes()
 
 function addListenerForEnText() {
     btnMain.removeEventListener('click', getQuotes)
